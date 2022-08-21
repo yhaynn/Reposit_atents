3a93414e95b6d3aad3f7b9a83aca1efcfaef81a9
yhaynn <yhaynn@gmail.com>
2022-08-21 13:58:50 +0900

    HvsO

---
+++ WeekendTask/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine (7<<2);
+
+
+            Console.ReadKey ();
+        }
+    }
+}
